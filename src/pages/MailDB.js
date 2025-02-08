f8809666a81275faf464ffd7d28a0b0a52572bdf
Vikas Rajpoot <120569817+vikasrajpoot30@users.noreply.github.com>
2025-02-08 21:19:20 +0530

    Update MailDB.js

--- src/pages/MailDB.js
+++ src/pages/MailDB.js
@@ -5,62 +5,78 @@ export const mails = [
     isStarred: false,
     subject: "Training Program",
     content:
-      "About Microsoft Virtual Academy<br/>Microsoft Virtual Academy provides free online training.",
+      "Enroll in our new skill-building workshop and get certified by industry experts.",
   },
   {
     mId: "guid-2",
     unread: true,
     isStarred: false,
-    subject: "Empower your future",
+    subject: "Empower Your Future",
     content:
-      "We foster our pipeline of future leaders with 47 employee networks and 7 global employment programs.",
+      "Join our leadership programs and gain insights from top professionals worldwide.",
   },
   {
     mId: "guid-3",
     unread: true,
     isStarred: true,
-    subject: "Pre Approved Loan",
+    subject: "Exclusive Loan Offer",
     content:
-      "Congratulations! <u>Credit card</u> is being shipped to you today!",
+      "You are eligible for a low-interest personal loan. Apply now to avail the offer!",
   },
   {
     mId: "guid-4",
     unread: true,
     isStarred: false,
-    subject: "You won a lottery!",
+    subject: "Lucky Draw Winner",
     content:
-      "You have just won the New York official lottery. Please send us your address so that we can process your winnings.",
+      "Congratulations! You have won an exciting prize in our grand lucky draw contest.",
   },
   {
     mId: "guid-5",
     unread: true,
     isStarred: false,
-    subject: "Invact Finance Program",
+    subject: "Finance Accelerator Program",
     content:
-      "About Microsoft Virtual Academy<br/>Microsoft Virtual Academy provides free online training.",
+      "Boost your financial knowledge with our expert-led training modules and courses.",
   },
   {
     mId: "guid-6",
     unread: true,
     isStarred: false,
-    subject: "neoG Camp Interview",
+    subject: "Tech Conference Invite",
     content:
-      "We foster our pipeline of future leaders with 47 employee networks and 7 global employment programs.",
+      "You're invited to the Global Tech Conference 2025. Reserve your spot now!",
   },
   {
     mId: "guid-7",
     unread: true,
     isStarred: false,
-    subject: "Pre Approved Loan 1",
+    subject: "Credit Card Pre-Approval",
     content:
-      "Congratulations! <u>Credit card</u> is being shipped to you today!",
+      "Get your premium credit card with zero annual fees and exciting cashback offers.",
   },
   {
     mId: "guid-8",
     unread: true,
     isStarred: false,
-    subject: "You won a lottery! 8th time",
+    subject: "Special Discount on Courses",
     content:
-      "You have just won the New York official lottery. Please send us your address so that we can process your winnings.",
+      "Unlock up to 50% off on our premium online courses. Offer valid for a limited time.",
+  },
+  {
+    mId: "guid-9",
+    unread: true,
+    isStarred: true,
+    subject: "Freelancing Opportunity",
+    content:
+      "Earn money working remotely! Join our freelancing network and access global projects.",
+  },
+  {
+    mId: "guid-10",
+    unread: true,
+    isStarred: false,
+    subject: "Cybersecurity Alert",
+    content:
+      "Protect your online accounts from phishing scams. Read our security guidelines now.",
   },
 ];
